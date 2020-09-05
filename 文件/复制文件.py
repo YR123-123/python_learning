8a81f6b6a6e2aae8af70872da38112a6b19eef0a
YR123-123 <yangrui99128@163.com>
2020-09-05 16:18:18 +0800

    更新学习

---
+++ 文件/复制文件.py
@@ -0,0 +1,15 @@
+# 1. 打开
+file_read = open("README")
+file_write = open("README[复件]", "w")
+
+# 2. 读、写
+while True:
+    #
+    text = file_read.readline()
+    if not text:
+        break
+    file_write.write(text)
+
+# 3. 关闭、
+file_read.close()
+file_write.close()
